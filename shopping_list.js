314a71a1da1fe18440db213e76f16c307256d699
Anthony Guy <anthonyyguyy@gmail.com>
2025-12-22 22:06:34 -0500

    Designing html features

---
+++ shopping_list.js
@@ -0,0 +1,11 @@
+const taskListDiv = document.getElementById('task-list');
+const taskInput = document.getElementById('tasks');
+const addTask = document.getElementById('add-task');
+
+
+addTask.addEventListener('click', (event) => {
+    event.preventDefault();
+    const taskText = taskInput.value.trim();
+    const 
+    }
+});
