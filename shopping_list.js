3198cf594cda8fe15798bf9d3a7eb71a177a8f54
Anthony Guy <anthonyyguyy@gmail.com>
2025-12-23 14:02:41 -0500

    Developing front end UI/UX and creating elements using DOM manipulation.

--- shopping_list.js
+++ shopping_list.js
@@ -1,11 +1,26 @@
-const taskListDiv = document.getElementById('task-list');
-const taskInput = document.getElementById('tasks');
-const addTask = document.getElementById('add-task');
+const listInput = document.getElementById('list');
+const addItemBtn = document.getElementById('add-item');
 
-
-addTask.addEventListener('click', (event) => {
+addItemBtn.addEventListener('click', (event) => {
     event.preventDefault();
-    const taskText = taskInput.value.trim();
-    const 
-    }
-});
+    const paper = document.querySelector('#paper');
+    const listText = listInput.value;
+    const paragraph = document.createElement('p')
+    const listDiv = document.createElement('div');
+    listDiv.setAttribute('class', 'list');
+    paragraph.setAttribute('id', 'list-item');
+    listDiv.innerHTML += `
+        <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" role="switch" id="switchCheckDefault">
+            <label class="form-check-label" for="switchCheckDefault">complete/incomplete</label>
+        </div>
+        <button class="delete-btn" id="delete-btn">DELETE</button>
+    `
+    paragraph.innerText = listText;
+    paper.appendChild(paragraph)
+    paper.appendChild(listDiv)
+    listInput.value = '';
+    });
+
+    // const 
+    const deleteBtn = document.getElementById('delete-btn');
