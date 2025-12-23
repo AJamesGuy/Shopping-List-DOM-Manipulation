56acfaa8a2d8140e32b39c4d6d1623d28080d1e9
Anthony Guy <anthonyyguyy@gmail.com>
2025-12-23 16:43:58 -0500

    Delete, Add Item, and Clear ALL buttons are functional with proper DOM manipulation.

--- shopping_list.js
+++ shopping_list.js
@@ -3,24 +3,46 @@ const addItemBtn = document.getElementById('add-item');
 
 addItemBtn.addEventListener('click', (event) => {
     event.preventDefault();
-    const paper = document.querySelector('#paper');
-    const listText = listInput.value;
-    const paragraph = document.createElement('p')
-    const listDiv = document.createElement('div');
-    listDiv.setAttribute('class', 'list');
-    paragraph.setAttribute('id', 'list-item');
-    listDiv.innerHTML += `
+    if (listInput.value == ''){
+        // Nothing
+    } else {
+
+        const paper = document.querySelector('#paper');
+        const listText = listInput.value;
+        const paragraph = document.createElement('p')
+        const listDiv = document.createElement('div');
+        listDiv.setAttribute('class', 'list');
+        listDiv.setAttribute('id', 'listDiv');
+        paragraph.setAttribute('id', 'list-item');
+        listDiv.innerHTML += `
         <div class="form-check form-switch">
-            <input class="form-check-input" type="checkbox" role="switch" id="switchCheckDefault">
-            <label class="form-check-label" for="switchCheckDefault">complete/incomplete</label>
+        <input class="form-check-input" type="checkbox" role="switch" id="switchCheckDefault">
+        <label class="form-check-label" for="switchCheckDefault">complete/incomplete</label>
         </div>
         <button class="delete-btn" id="delete-btn">DELETE</button>
-    `
-    paragraph.innerText = listText;
-    paper.appendChild(paragraph)
-    paper.appendChild(listDiv)
-    listInput.value = '';
+        `
+        paragraph.innerText = `List item: ${listText}`;
+        paper.appendChild(paragraph)
+        paper.appendChild(listDiv)
+        listInput.value = '';
+    }
     });
 
-    // const 
-    const deleteBtn = document.getElementById('delete-btn');
+        
+document.addEventListener('click', (e) =>{
+        if (e.target.id === 'delete-btn'){
+            const listitem = document.getElementById('list-item');
+            e.target.parentElement.remove()
+            listitem.remove()
+        }
+
+});
+        
+
+document.addEventListener('click', (e) =>{
+    e.preventDefault()
+    if (e.target.id === 'clear-all'){
+        const paper = document.querySelector('#paper');
+        paper.innerHTML = `<h2 class="title">--- Shopping List ---</h2>` 
+    }
+})
