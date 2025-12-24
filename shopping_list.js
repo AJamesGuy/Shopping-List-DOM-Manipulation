0377937468695edf716db173387bfb03a7172388
Anthony Guy <anthonyyguyy@gmail.com>
2025-12-24 15:01:50 -0500

    Modified buttons and and refactored DOM manipulation functions. Added css for styling.

--- shopping_list.js
+++ shopping_list.js
@@ -1,6 +1,9 @@
 const listInput = document.getElementById('list');
 const addItemBtn = document.getElementById('add-item');
 
+let total = 0;
+let remaining = 0;
+let completed = 0;
 addItemBtn.addEventListener('click', (event) => {
     event.preventDefault();
     if (listInput.value == ''){
@@ -10,45 +13,39 @@ addItemBtn.addEventListener('click', (event) => {
         const paper = document.querySelector('#paper');
         const listText = listInput.value;
         paper.innerHTML += `
-        <p id="list-item">${listText}</p>
+        <div class="shopping-item">
+        <h2 id="list-item">${listText}</h2>
         <div class="buttons" id="buttons">
         <button class="completed-Btn" id="complete">Complete</button>
-        <button class="delete-btn" id="delete">DELETE</button>
+        </div>
         </div>`
-        // const paragraph = document.createElement('p')
-        // const listDiv = document.createElement('div');
-        // listDiv.setAttribute('class', 'list');
-        // listDiv.setAttribute('id', 'listDiv');
-        // paragraph.setAttribute('id', 'list-item');
-        // listDiv.innerHTML += `
-        // <div class="form-check form-switch">
-        // <input class="form-check-input" type="checkbox" role="switch" id="switchCheckDefault">
-        // <label class="form-check-label" for="switchCheckDefault">complete/incomplete</label>
-        // </div>
-        // <button class="delete-btn" id="delete-btn">DELETE</button>
-        // `
-        // paragraph.innerText = `- ${listText}`;
-        // paper.appendChild(paragraph)
-        // paper.appendChild(listDiv)
         listInput.value = '';
+        total += 1;
+        remaining += 1;
+        stats()
     }
     });
-
         
-document.addEventListener('click', (e) =>{
-        if (e.target.id === 'delete'){
-            e.target.parentElement.remove()
-            const par = document.getElementById('list-item');
-            par.remove()
+document.addEventListener('click', (e =>{
+    if (e.target.id === 'clear-completed'){
+        const completeBtns = document.getElementsByClassName('completed-Btn');
+        Array.from(completeBtns).forEach(button =>{
+            if (button.innerText === 'Incomplete'){
+                button.parentElement.parentElement.remove()
+            }
+        })
     }
-});
-        
+}))
 
 document.addEventListener('click', (e) =>{
     e.preventDefault()
     if (e.target.id === 'clear-all'){
         const paper = document.querySelector('#paper');
-        paper.innerHTML = `<h2 class="title">--- Shopping List ---</h2>` 
+        paper.innerHTML = `<h2 class="title">Shopping List</h2>`
+        total = 0;
+        remaining = 0;
+        completed = 0;
+        stats()
     }
 })
 
@@ -59,9 +56,27 @@ document.addEventListener('click', (e) => {
         if (e.target.textContent === 'Complete'){
             paragraph.style.textDecoration = 'line-through';
             e.target.textContent = 'Incomplete';
+            remaining -= 1;
+            completed += 1;
         } else {
             paragraph.style.textDecoration = 'none';
             e.target.textContent = 'Complete';
+            remaining += 1;
+            completed -= 1;
         }
+        stats()
     }
 })
+
+const stats = () => {
+    const totalCount = document.getElementsByTagName('h4')[0];
+    const remainingCount = document.getElementsByTagName('h4')[1];
+    const completedCount = document.getElementsByTagName('h4')[2];
+    const stats = document.getElementsByClassName('stats')[0];
+    totalCount.innerText = `Total: ${total}`;
+    remainingCount.innerText = `Remaining: ${remaining}`;
+    completedCount.innerText = `Completed: ${completed}`;
+    stats.appendChild(totalCount);
+    stats.appendChild(remainingCount);
+    stats.appendChild(completedCount);
+}
